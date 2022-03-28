44591c2a45eb7b86f9a228f43550ea1f42a1cc90
Airyclon <84323073+Airyclon@users.noreply.github.com>
2022-03-28 17:03:20 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+
+function Start(){
+    Webcam.set({
+        width:360,
+        height:250,
+        image_format : 'png',
+        png_quality:90
+    });
+    camera = document.getElementById("camera");
+    Webcam.attach(camera);
+}
